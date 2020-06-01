6040a1170cdd3cf7ab82c11f21e7d2da708dfb00
Ole Øyvind Hove <oleo@vanilla.no>
2020-06-02 00:18:13 +0200

    added first working version.

--- redisconfig.go
+++ redisconfig.go
@@ -1,12 +1,9 @@
 package redisconfig
 import(
 	"encoding/json"
-	"flag"
 	"log"
-//	"strings"
 	"context"
-	"fmt"
-	"os"
+	//"fmt"
 	"github.com/go-redis/redis"
 )
 type StringConfig struct {
@@ -26,33 +23,34 @@ type Init struct {
 	Host string
 }
 type conKey string
-func get(key string) {
+
+func (e *Init) Get(key string) StringConfig {
 	k := conKey("jalla")
 	ctx := context.WithValue(context.Background(),k, "Goredisssss")
 	// get connection to redis and get json-config
-	cli := rClient(Host)
-	err = ping(ctx,cli)
+	cli := rClient(e.Host)
+	err := ping(ctx,cli)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	raw_config := getstr(ctx,cli,key)
 //	"circus:test-string-1:config")
-	fmt.Printf("Got config:\n%s\n-\n",raw_config)
+	//fmt.Printf("Got config:\n%s\n-\n",raw_config)
 	Config := StringConfig{}
 	err = json.Unmarshal([]byte(raw_config), &Config)
 	if err != nil {
 		log.Fatal("Can't deode config JSON: ",err)
 	}
-	fmt.Println("Will be using:")
-	fmt.Printf("Host:   %s:%d\n",Config.MessageBus.Host,Config.MessageBus.Port)
-	fmt.Print("Topic:   ")
-	fmt.Println(Config.MessageBus.Topic)
+	//fmt.Println("Will be using:")
+	//fmt.Printf("Host:   %s:%d\n",Config.MessageBus.Host,Config.MessageBus.Port)
+	//fmt.Print("Topic:   ")
+	//fmt.Println(Config.MessageBus.Topic)
 
-
+	return Config
 }
 
 func rClient(host string) *redis.Client {
-	fmt.Printf("Will conntact redis at %s\n",host)
+	//fmt.Printf("Will conntact redis at %s\n",host)
 	client := redis.NewClient(&redis.Options{
 		Addr: host,
 		Password: "",
@@ -63,21 +61,20 @@ func rClient(host string) *redis.Client {
 }
 
 func ping(ctx context.Context, client *redis.Client) error {
-	pong, err := client.Ping(ctx).Result()
+	_ , err := client.Ping(ctx).Result()
 	if err != nil {
 		return err
 	}
-	fmt.Println(pong,err)
 
 	return nil
 }
 
 func getstr(ctx context.Context, client *redis.Client,key string) string {
 	out:=""
-	fmt.Printf("Will try to get %s\n",key)
+//	fmt.Printf("Will try to get %s\n",key)
 	Val, err := client.Get(ctx,key).Result()
 	if err == redis.Nil {
-		fmt.Println("no value found")
+		log.Println("no value found")
 	} else if err != nil {
 		panic(err)
 	} else {
